3c0eb181e07ff0f6c34ff8407067b3d4144549a6
Vladyslav Tkachenko <vladennopwnz@gmail.com>
2025-11-23 01:52:02 +0000

    Added task 7

---
+++ CPP #7/CPP.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+void printAdd(int a, int b) {
+	std::cout << "Add: " << (a + b) << std::endl;
+};
+
+void printSub(int a, int b) {
+	std::cout << "Sub: " << (a - b) << std::endl;
+}
+
+void printMultiplicate(int a, int b) {
+	std::cout << "Multiplicate: " << (a * b) << std::endl;
+}
+
+void printDivision(int a, int b) {
+	if (a == 0 || b == 0) {
+		std::cout << "Cannot divide by zero!" << std::endl;
+		return;
+	}
+	std::cout << "Divide: " << (a / b) << std::endl;
+}
+
+
+int main()
+{
+	int a, b;
+	std::cout << "Enter first integer: ";
+	std::cin >> a;
+
+	std::cout << "Enter second integer: ";
+	std::cin >> b;
+
+	printAdd(a, b);
+
+	printSub(a, b);
+
+	printMultiplicate(a, b);
+
+	printDivision(a, b);
+}
+
+
